59986f5f2451fd9f7cf7eb9f2e3f8b1315dbbf72
kunukulavanya03 <kunukulavanya03@gmail.com>
2025-12-12 15:57:06 +0530

    🚀 Initial commit - Generated with CODE AGENT

---
+++ backend/app/database.py
@@ -0,0 +1,10 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import declarative_base, scoped_session
+from sqlalchemy.orm.session import sessionmaker
+from app.config import settings
+
+engine = create_engine(settings.DATABASE_URL)
+
+Base = declarative_base()
+
+SessionLocal = sessionmaker(bind=engine)
